559c252507fda2961560cdc40a2db01a87c5c127
alrickdemetrius <alrick.demetrius@gmail.com>
2025-07-21 16:46:34 +0700

    Landing Page edit

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,17 +1,36 @@
 @extends('layouts.app')
-
+<style>
+    .logo-img {
+        height: calc(1em * 1.2);
+        width: auto;
+        vertical-align: middle;
+        margin: 0 0.1em;
+    }
+</style>
 @section('content')
-<div class="bg-light min-vh-100 d-flex align-items-center justify-content-center">
-    <div class="container text-center py-5">
-        <h1 class="display-4 fw-bold mb-4 text-dark">Welcome to Wepeka</h1>
-        <p class="lead text-secondary mb-5">
-            A platform for managing events, QR code attendance, and more.
-        </p>
-
-        <div class="d-flex justify-content-center gap-3">
-            <a href="{{ route('login') }}" class="btn btn-dark btn-lg px-4 rounded-pill shadow">Login</a>
+    <div class="bg-light min-vh-100 d-flex align-items-center justify-content-center">
+        <div class="container text-center py-5">
+            <h1 class="display-4 fw-bold mb-4 text-dark">
+                Welcome to <span class="text-warning fw-semibold">We</span><span class="fw-semibold">peka</span> Site</h1>
+            <h2 class="mb-5 fw-bold">
+                Identity That Inspires
+            </h2>
+            @if(Auth::user())
+                <div class="d-flex justify-content-center gap-3">
+                    <a href="{{ route('client.headquarters') }}"
+                        class="btn btn-dark btn-lg px-4 rounded-pill shadow">Headquarters</a>
+                </div>
+                <div class="d-flex justify-content-center gap-3 mb-5">
+                    <p class="lead text-secondary mb-5">
+                        A platform for managing events, QR code, and more.
+                    </p>
+                </div>
+            @else
+                <div class="d-flex justify-content-center gap-3">
+                    <a href="{{ route('login') }}" class="btn btn-dark btn-lg px-4 rounded-pill shadow">Login</a>
+                </div>
+            @endif
 
         </div>
     </div>
-</div>
 @endsection
